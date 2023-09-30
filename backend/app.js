fa13b80be6ebfcfdc3931a91186707c5eb73197d
Ghulam Mohiyuddin <subahani98@gmail.com>
2023-09-30 22:33:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,11 @@ const cors = require("cors")
 const app = express()
 //json parser
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+        origin: ["https://shl-assignment-mern.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }))
 
 //adding routes
 const project = require("./routes/projectRoute")
